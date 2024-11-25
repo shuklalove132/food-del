ab804548a4e777382d5912d49d3f6d4710b25fd2
shuklalove132 <shuklalove132@gmail.com>
2024-11-25 12:19:14 +0530

    firebase error check 1

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 5174 },
+  rollupOptions: {
+    external: ['firebase/storage'],
+  },
 })
