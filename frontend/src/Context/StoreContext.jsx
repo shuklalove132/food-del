927c440f547a30747fff9e895eaed64d1d46a803
shuklalove132 <32066857+shuklalove132@users.noreply.github.com>
2024-11-25 10:42:08 +0530

    Update backend URL of StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000";
+    const url = "https://ilcibo-backend.onrender.com";
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("");
