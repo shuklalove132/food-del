0f61f81b52da62195a62df28ad64ecbb89030206
love prath shukla <shuklalove132@gmail.com>
2024-12-10 11:31:38 +0530

    lets see

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  include: ['firebase/compat/app', 'firebase/compat/auth', 'firebase/compat/firestore'],
+  rollupOptions: ['firebase/compat/app', 'firebase/compat/auth', 'firebase/compat/firestore'],
 },
 )
