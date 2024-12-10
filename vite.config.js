0f840340f257a82b9c7983f56021808c48b31851
love prath shukla <shuklalove132@gmail.com>
2024-12-10 10:59:03 +0530

    firebase config changeddd

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rollupOptions: {
-    external: ['firebase/app', 'firebase/storage'],
-  },
-})
+  include: ['firebase/compat/app', 'firebase/compat/auth', 'firebase/compat/firestore'],
+},
+)
